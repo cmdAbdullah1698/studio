e78ddfa51eab77136eef69df20eea47864b3d9e4
Abdullah Bin Athar <abinathar1698@gmail.com>
2025-07-30 15:07:56 +0000

    change the background color for all shards to white (major) and blue (mi

--- src/components/background-shards.tsx
+++ src/components/background-shards.tsx
@@ -18,21 +18,19 @@ const Shard = ({
 export default function BackgroundShards() {
   return (
     <div className="fixed inset-0 -z-20 overflow-hidden bg-background">
-      {/* 50% Blue Shards */}
-      <Shard className="top-[-15%] left-[-10%] w-1/2 h-1/2 transform rotate-12" colorClass="fill-primary/70" />
-      <Shard className="bottom-[5%] left-[50%] w-1/3 h-1/3 transform -rotate-45" colorClass="fill-primary/40" />
-      <Shard className="bottom-[-20%] left-[5%] w-1/2 h-1/2 transform rotate-45" colorClass="fill-primary/80" />
-      <Shard className="top-[40%] left-[20%] w-1/5 h-1/5 transform -rotate-20" colorClass="fill-primary/60" />
-      <Shard className="top-[-10%] right-[10%] w-1/4 h-1/4 transform rotate-30" colorClass="fill-primary/50" />
-
-      {/* 20% Yellow Shards */}
-      <Shard className="top-[15%] right-[-10%] w-1/3 h-1/3 transform -rotate-12" colorClass="fill-accent/60" />
-      <Shard className="bottom-[-5%] right-[-10%] w-2/5 h-2/5 transform -rotate-30" colorClass="fill-accent/70" />
-      
-      {/* 30% White Shards */}
+      {/* White Shards (Major) */}
+      <Shard className="top-[-15%] left-[-10%] w-1/2 h-1/2 transform rotate-12" colorClass="fill-foreground/10" />
+      <Shard className="bottom-[5%] left-[50%] w-1/3 h-1/3 transform -rotate-45" colorClass="fill-foreground/5" />
+      <Shard className="bottom-[-20%] left-[5%] w-1/2 h-1/2 transform rotate-45" colorClass="fill-foreground/15" />
+      <Shard className="top-[40%] left-[20%] w-1/5 h-1/5 transform -rotate-20" colorClass="fill-foreground/10" />
+      <Shard className="top-[-10%] right-[10%] w-1/4 h-1/4 transform rotate-30" colorClass="fill-foreground/5" />
       <Shard className="top-[50%] left-[10%] w-1/5 h-1/5 transform rotate-60" colorClass="fill-foreground/10" />
       <Shard className="top-[60%] right-[15%] w-1/3 h-1/3 transform rotate-15" colorClass="fill-foreground/5" />
       <Shard className="top-[10%] left-[40%] w-1/6 h-1/6 transform rotate-15" colorClass="fill-foreground/10" />
+
+      {/* Blue Shards (Minor) */}
+      <Shard className="top-[15%] right-[-10%] w-1/3 h-1/3 transform -rotate-12" colorClass="fill-primary/50" />
+      <Shard className="bottom-[-5%] right-[-10%] w-2/5 h-2/5 transform -rotate-30" colorClass="fill-primary/60" />
     </div>
   );
 }
